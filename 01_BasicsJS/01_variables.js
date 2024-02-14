66e39dc721e594c3a6059f36b5b380897860a5c3
Hardik Teotia <130360009+hardikteotia@users.noreply.github.com>
2024-02-14 10:06:03 +0000

    vars and datatypes

---
+++ 01_BasicsJS/01_variables.js
@@ -0,0 +1,7 @@
+const id = 2736
+let email = "my@email.com"
+var city = "gzb"
+password = 238762
+
+// console.log(id)
+console.table([id, email, city, password])
---
+++ 01_BasicsJS/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";//treat js code as newer version
+
+let num = 874
+let name = "myname"
+let isTrue = false
+// ek hota hai undefined like (let name;)
+// or ek hota hai null which makes string null.
+
+//object
+
+console.log(typeof null);
