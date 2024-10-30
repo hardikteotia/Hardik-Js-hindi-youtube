bf00e340c16cbb2f5f4880a80586f7bd8083be0b
Hardik Teotia <130360009+hardikteotia@users.noreply.github.com>
2024-10-30 05:44:47 +0000

    study

--- 01_BasicsJS/05_numsAndMaths.js
+++ 01_BasicsJS/05_numsAndMaths.js
@@ -9,7 +9,7 @@ console.log(num1.toFixed(2));
 
 const anotherNum = 1000000000000
 console.log(anotherNum.toLocaleString());//as per US standards
-console.log(anotherNum.toLocaleString('en-IN'));//as per INDIA standards
+console.log(anotherNum.toLocaleString('en-IN'));//as per INDIAN standards
 
 // Math functions are: floor, ceil, round, Min, Max, random, etc
 console.log((Math.random()*20).toFixed(0));
@@ -18,3 +18,5 @@ const min = 10
 const max = 20
 
 console.log(Math.floor(Math.random()*(max-min+1)+ min))
+console.log(Math.min(3,4,5,6));
+console.log(Math.max(3,4,5,6));
