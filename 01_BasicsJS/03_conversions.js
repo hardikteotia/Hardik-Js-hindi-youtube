01ab66d92adeb8c5d8a303aaf596670646e5d789
Hardik Teotia <130360009+hardikteotia@users.noreply.github.com>
2024-10-30 04:48:16 +0000

    changes

--- 01_BasicsJS/03_conversions.js
+++ 01_BasicsJS/03_conversions.js
@@ -1,10 +1,10 @@
 let score = "33abs"
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
 //convert 33 into Numbers?? yes
@@ -13,4 +13,13 @@ console.log(valueInNumber);
 
 let isBool = 1
 let bool = Boolean(isBool)
-console.log(bool);
+//console.log(bool);
+
+console.log("2" < 3);
+console.log("2" < "3");
+console.log("22" == 3);
+console.log(null == 0 );//explaination below
+// Comparison Steps:
+
+// When comparing null to 0, JavaScript first checks if the two values are of the same type. Since null is an object and 0 is a number, it tries to coerce one or both values to a common type.
+// In this case, null is not converted to a number when compared to 0, and thus the comparison null == 0 evaluates to false.
